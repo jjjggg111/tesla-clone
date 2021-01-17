d0dae43915ebb650cbeb987be4877bdf80935c38
jjjggg111 <thisisjungjiwon@gmail.com>
2021-01-18 02:35:06 +0900

    button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,94 @@
 import './App.css';
 import Header from './components/Header';
+import Item from './components/Item';
+
+import Accessories from './assets/Accessories.jpg';
+import ModelS from './assets/ModelS.jpeg';
+import Model3 from './assets/Model3.jpeg';
+import ModelX from './assets/ModelX.jpeg';
+import ModelY from './assets/ModelY.jpeg';
+import SolarPanel from './assets/SolarPanels.jpeg';
+import SolarRoof from './assets/SolarRoof.jpeg';
 
 function App() {
   return (
     <div className="App">
       {/*header */}
       <Header />
-      {/*item 1 */}
+      <div className="app__itemsContainer">
+        <Item
+          title="Lowest Cost Solar Panels in America"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanel}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          RightBtnTxt="LEARN MORE"
+          TwoButtons="true"
+          first
+        />
+        <Item
+          title="Model S"
+          desc="Starting at $69,420"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt="CUSMTOM ORDER"
+          leftBtnLink=""
+          RightBtnTxt="EXISTING INVENTORY"
+          TwoButtons="true"
+        />
+        <Item
+          title="Model 3"
+          desc="Order Online for Touchless Delivery"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnTxt="CUSMTOM ORDER"
+          leftBtnLink=""
+          RightBtnTxt="EXISTING INVENTORY"
+          TwoButtons="true"
+        />
+        <Item
+          title="Model X"
+          desc="Order Online for Touchless Delivery"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt="CUSMTOM ORDER"
+          leftBtnLink=""
+          RightBtnTxt="EXISTING INVENTORY"
+          TwoButtons="true"
+        />
+        <Item
+          title="Model Y"
+          desc="Order Online for Touchless Delivery"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="CUSMTOM ORDER"
+          leftBtnLink=""
+          RightBtnTxt="LEARN MORE"
+          TwoButtons="true"
+        />
+        <Item
+          title="Solar for New Roofs"
+          desc="
+          Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+          descLink=""
+          backgroundImg={SolarRoof}
+          leftBtnTxt="CUSMTOM ORDER"
+          leftBtnLink=""
+          RightBtnTxt="LEARN MORE"
+          TwoButtons="true"
+        />
+        <Item
+          title="Accessories"
+          desc=""
+          descLink=""
+          backgroundImg={Accessories}
+          leftBtnTxt="SHOP NOW"
+          leftBtnLink=""
+          RightBtnTxt="LEARN MORE"
+          TwoButtons="true"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,49 @@
+import { findAllByTitle } from '@testing-library/react';
+import React from 'react';
+import Button from './Button';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import './Item.css';
+
+const item = ({
+  title,
+  desc,
+  backgroundImg,
+  leftBtnTxt,
+  leftBtnLink,
+  RightBtnTxt,
+  RightBtnLink,
+  twoButtons,
+  first,
+}) => {
+  return (
+    <div className="item" style={{ backgroundImage: `url(${backgroundImg})` }}>
+      <div className="item__container">
+        <div className="item__text">
+          <p>{title}</p>
+          <div className="item__textDesc">
+            <p>{desc}</p>
+          </div>
+          <div className="item__lowerThird">
+            <div className="item__buttons">
+              <Button imp="primary" text={leftBtnTxt} link={leftBtnLink} />
+              {twoButtons && (
+                <Button
+                  imp="secondary"
+                  text={RightBtnTxt}
+                  link={RightBtnLink}
+                />
+              )}
+            </div>
+            {first && (
+              <div className="item__expand">
+                <ExpandMoreIcon />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default item;
