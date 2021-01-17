e33785fa93f6d8d05334f17719de373bea173843
jjjggg111 <thisisjungjiwon@gmail.com>
2021-01-17 23:44:44 +0900

    nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      {/*header */}
+      <Header />
+      {/*item 1 */}
     </div>
   );
 }
